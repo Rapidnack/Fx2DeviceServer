0782f6072c9226b78fa3e150943566b7e6e31c04
rapidnack <rapidnack@gmail.com>
2020-04-12 11:36:38 +0900

    first commit

---
+++ Fx2DeviceServer/DACDevice.cs
@@ -0,0 +1,140 @@
+﻿using CyUSB;
+using MonoLibUsb;
+using MonoLibUsb.Profile;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Fx2DeviceServer
+{
+    public class DACDevice : Fx2Device
+    {
+        private ushort dataPortNo = 0;
+        private CyBulkEndPoint endpoint2 = null;
+
+        public DACDevice(CyUSBDevice usbDevice, MonoUsbProfile usbProfile)
+            : base(usbDevice, usbProfile, EDeviceType.DAC)
+        {
+            byte[] response = ReceiveVendorResponse(VendorDeviceParam, 2);
+            dataPortNo = (ushort)(response[0] + (response[1] << 8));
+
+            Console.WriteLine($"DeviceAttached: {this}");
+
+            if (USBDevice != null)
+            {
+                endpoint2 = USBDevice.EndPointOf(0x02) as CyBulkEndPoint;
+            }
+
+            Cts = new CancellationTokenSource();
+            var ct = Cts.Token;
+            Task.Run(() =>
+            {
+                try
+                {
+                    while (!ct.IsCancellationRequested)
+                    {
+                        TcpClient client = null;
+                        try
+                        {
+                            client = new TcpClient("127.0.0.1", dataPortNo);
+                        }
+                        catch (SocketException ex)
+                        {
+                            if (ex.ErrorCode == 10061)
+                            {
+                                Thread.Sleep(1000);
+                                continue;
+                            }
+
+                            throw ex;
+                        }
+                        Console.WriteLine("{0}: data connection accepted({1}:{2}).",
+                            DeviceType,
+                            ((IPEndPoint)client.Client.RemoteEndPoint).Address,
+                            ((IPEndPoint)client.Client.RemoteEndPoint).Port);
+
+                        NetworkStream ns = client.GetStream();
+                        try
+                        {
+                            int maxPacketSize;
+                            if (USBDevice != null)
+                            {
+                                maxPacketSize = endpoint2.MaxPktSize;
+                            }
+                            else
+                            {
+                                maxPacketSize = MonoUsbApi.GetMaxPacketSize(USBProfile.ProfileHandle, 0x02);
+                            }
+                            byte[] inData = new byte[64 * 1024];
+                            byte[] outData = new byte[maxPacketSize];
+                            int outDataPos = 0;
+
+                            while (!ct.IsCancellationRequested)
+                            {
+                                int resSize = ns.Read(inData, 0, inData.Length);
+                                if (resSize == 0)
+                                    break;
+
+                                int inDataLen = resSize;
+                                int inDataPos = 0;
+                                while (!ct.IsCancellationRequested && inDataPos < inDataLen)
+                                {
+                                    while (outDataPos < outData.Length && inDataPos < inDataLen)
+                                    {
+                                        outData[outDataPos++] = inData[inDataPos++];
+                                    }
+
+                                    if (outDataPos == outData.Length)
+                                    {
+                                        int xferLen = outData.Length;
+                                        bool ret = false;
+                                        if (USBDevice != null)
+                                        {
+                                            ret = endpoint2.XferData(ref outData, ref xferLen);
+                                        }
+                                        else
+                                        {
+                                            ret = MonoUsbApi.BulkTransfer(MonoDeviceHandle, 0x02, outData, outData.Length, out xferLen, TIMEOUT) == 0;
+                                        }
+                                        if (ret == false || xferLen == 0)
+                                            break;
+                                        if (xferLen != outData.Length)
+                                        {
+                                            Console.WriteLine($"{DeviceType}: the response size {xferLen} not equal to the requested size {outData.Length}");
+                                        }
+                                        outDataPos = 0;
+                                    }
+                                }
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            // nothing to do
+                        }
+                        finally
+                        {
+                            ns.Close();
+                            client.Close();
+                            Console.WriteLine($"{DeviceType}: data connection closed.");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // nothing to do
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{DeviceType}: {ex.Message}");
+                }
+            }, ct);
+        }
+
+        public override string ToString()
+        {
+            return $"{DeviceType} {dataPortNo}";
+        }
+    }
+}
---
+++ Fx2DeviceServer/Fx2Device.cs
@@ -0,0 +1,164 @@
+﻿using CyUSB;
+using MonoLibUsb;
+using MonoLibUsb.Profile;
+using System;
+using System.Threading;
+
+namespace Fx2DeviceServer
+{
+    public class Fx2Device : IDisposable
+    {
+        public enum EDeviceType
+        {
+            Unknown = 0,
+            DAC = 1,
+            ADC = 2,
+        }
+
+        protected const int TIMEOUT = 3000;
+        public const byte VendorDeviceType = 0xc0;
+        public const byte VendorDeviceParam = 0xc1;
+
+        protected EDeviceType DeviceType { get; private set; } = EDeviceType.Unknown;
+        protected CancellationTokenSource Cts { get; set; } = new CancellationTokenSource();
+        protected MonoUsbDeviceHandle MonoDeviceHandle { get; private set; } = null;
+
+        public CyUSBDevice USBDevice { get; private set; } = null;
+
+        private MonoUsbProfile _usbProfile = null;
+        public MonoUsbProfile USBProfile
+        {
+            get
+            {
+                return _usbProfile;
+            }
+            set
+            {
+                _usbProfile = value;
+                MonoDeviceHandle = _usbProfile.OpenDeviceHandle();
+            }
+        }
+
+        public Fx2Device(CyUSBDevice usbDevice, MonoUsbProfile usbProfile, EDeviceType deviceType = EDeviceType.Unknown)
+        {
+            if (usbDevice != null)
+            {
+                USBDevice = usbDevice;
+            }
+            if (usbProfile != null)
+            {
+                USBProfile = usbProfile;
+            }
+            DeviceType = deviceType;
+
+            if (deviceType == EDeviceType.Unknown)
+            {
+                Console.WriteLine($"DeviceAttached: {this}");
+            }
+        }
+
+        private bool disposed = false;
+        public void Dispose()
+        {
+            Dispose(true);
+        }
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    if (Cts != null)
+                    {
+                        Cts.Cancel();
+                    }
+                    Console.WriteLine($"DeviceRemoved: {this}");
+                }
+
+                disposed = true;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{DeviceType}";
+        }
+
+        public static byte[] ReceiveVendorResponse(CyUSBDevice usbDevice, MonoUsbDeviceHandle monoDeviceHandle, byte reqCode, int length, ushort value = 0, ushort index = 0)
+        {
+            if (usbDevice != null)
+            {
+                CyControlEndPoint ctrlEpt = usbDevice.ControlEndPt;
+                ctrlEpt.TimeOut = TIMEOUT;
+                ctrlEpt.Direction = CyConst.DIR_FROM_DEVICE;
+                ctrlEpt.ReqType = CyConst.REQ_VENDOR;
+                ctrlEpt.Target = CyConst.TGT_DEVICE;
+                ctrlEpt.ReqCode = reqCode;
+                ctrlEpt.Value = value;
+                ctrlEpt.Index = index;
+
+                int bytes = length;
+                byte[] buffer = new byte[bytes];
+                ctrlEpt.XferData(ref buffer, ref bytes);
+                if (bytes == buffer.Length)
+                {
+                    return buffer;
+                }
+            }
+            else
+            {
+                short bytes = (short)length;
+                byte[] data = new byte[bytes];
+                byte requestType = CyConst.DIR_FROM_DEVICE + CyConst.REQ_VENDOR + CyConst.TGT_DEVICE;
+                int ret = MonoUsbApi.ControlTransfer(monoDeviceHandle, requestType, reqCode, (short)value, (short)index, data, bytes, TIMEOUT);
+                if (ret == data.Length)
+                {
+                    return data;
+                }
+            }
+
+            return null;
+        }
+
+        public static bool SendVendorRequest(CyUSBDevice usbDevice, MonoUsbDeviceHandle monoDeviceHandle, byte reqCode, byte[] data, ushort value = 0, ushort index = 0)
+        {
+            if (data == null)
+            {
+                data = new byte[0];
+            }
+
+            if (usbDevice != null)
+            {
+                CyControlEndPoint ctrlEpt = usbDevice.ControlEndPt;
+                ctrlEpt.TimeOut = TIMEOUT;
+                ctrlEpt.Direction = CyConst.DIR_TO_DEVICE;
+                ctrlEpt.ReqType = CyConst.REQ_VENDOR;
+                ctrlEpt.Target = CyConst.TGT_DEVICE;
+                ctrlEpt.ReqCode = reqCode;
+                ctrlEpt.Value = value;
+                ctrlEpt.Index = index;
+
+                int bytes = data.Length;
+                ctrlEpt.XferData(ref data, ref bytes);
+                return bytes == data.Length;
+            }
+            else
+            {
+                short bytes = (short)data.Length;
+                byte requestType = CyConst.DIR_TO_DEVICE + CyConst.REQ_VENDOR + CyConst.TGT_DEVICE;
+                int ret = MonoUsbApi.ControlTransfer(monoDeviceHandle, requestType, reqCode, (short)value, (short)index, data, bytes, TIMEOUT);
+                return ret == data.Length;
+            }
+        }
+
+        protected byte[] ReceiveVendorResponse(byte reqCode, int length, ushort value = 0, ushort index = 0)
+        {
+            return ReceiveVendorResponse(USBDevice, MonoDeviceHandle, reqCode, length, value, index);
+        }
+
+        protected bool SendVendorRequest(byte reqCode, byte[] data, ushort value = 0, ushort index = 0)
+        {
+            return SendVendorRequest(USBDevice, MonoDeviceHandle, reqCode, data, value, index);
+        }
+    }
+}
